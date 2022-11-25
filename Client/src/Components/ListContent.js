76dc849ad3734c5340076781478eacf74e70cf29
Jaypalsinh-Ramlavat <72656308+Jaypalsinh-Ramlavat@users.noreply.github.com>
2022-11-25 12:13:42 -0500

    Add files via upload

---
+++ Client/src/Components/ListContent.js
@@ -0,0 +1,24 @@
+const genre = [
+    {
+        title: 'Pop',
+        content: [ 'Listen to the latest pop songs' ]
+    },{
+        title: 'Rock',
+        content: ['Let\'s  Rock n Roll🎉']
+    },{
+        title: 'Jazz',
+        content: ['Enjoy some smooth jazz❤']
+    }, {
+        title: 'Hip Hop',
+        content: ['Start the boom box']
+    }, {
+        title: 'EDM',
+        content: ['Party time🎶']
+    }
+]
+
+const artist = []
+
+const developer = []
+
+export  {genre,artist, developer};
